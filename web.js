34f971217dcd91b8dbddeff9652391c933f886eb
Lyall Hamilton <lyall.hamilton@gmail.com>
2013-07-29 05:16:30 +0000

    First attempt to modify web.js to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var index_buf = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index_buf.toString());
 });
 
 var port = process.env.PORT || 5000;
